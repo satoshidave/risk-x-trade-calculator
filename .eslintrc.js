812ffce343ed42f6cc608bffd85e516e8aa4ae3e
David Porras <david.porras12@gmail.com>
2019-08-03 18:33:21 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": 0,
+        "indent": [2, 2],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled, { css } from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+`
+
+const CardContainer = styled.div`
+  ${({ width }) => width && css`
+    width: ${ width };
+  ` }
+  background-color: #1e1e1e;
+  border-bottom: 0.1rem solid rgba(255,255,255,.12);
+  box-shadow:
+    0 0.2rem 0.1rem -0.1rem rgba(0,0,0,.2),
+    0 0.1rem 0.1rem 0 rgba(0,0,0,.14),
+    0 0.1rem 0.3rem 0 rgba(0,0,0,.12);
+  border-radius: 0.4rem;
+`
+
+const CardHeader = styled.div`
+  background-color: #27272b;
+  height: 3.5rem;
+  border-radius: 0.4rem 0.4rem 0 0;
+  padding: 0.9rem;
+`
+const CardBody = styled.div`
+  padding: 0.9rem;
+`
+
+const Card = () => (
+  <CardContainer>
+    <CardHeader>Header</CardHeader>
+    <CardBody>
+      Body
+    </CardBody>
+  </CardContainer>
+)
+
+const SidebarContainer = styled.div`
+  width: 300px;
+  padding: 2rem;
+`
+
+const Sidebar = () => (
+  <SidebarContainer>
+    <Card />
+  </SidebarContainer>
+)
+
+const AppContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+`
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <AppContainer>
+        <Sidebar />
+        <Container>
+          works
+        </Container>
+      </AppContainer>
+    )
+  }
 }
-
-export default App;
